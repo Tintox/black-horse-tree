d4062faf5e3dd3c9809b5fb3ee6828550db170cc
Nelson Gutiérrez <tintolios@gmail.com>
2024-06-05 12:58:30 +0000

    change comment

--- comments.js
+++ comments.js
@@ -1,86 +1,82 @@
-//create new server
+//create web server
 const express = require('express');
-const router = express.Router();
-const Comment = require('../models/comment');
-const Post = require('../models/post');
-const User = require('../models/user');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const fs = require('fs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//get all comments
-router.get('/', (req, res) => {
-    Comment.find({}, (err, comments) => {
-        if (err) {
-            res.send('Error has occurred');
-        } else {
-            res.send(comments);
-        }
+//create a comment file
+var filePath = path.join(__dirname, 'comments.json');
+fs.stat(filePath, function(err, stat) {
+  if(err == null) {
+    console.log('File exists');
+  } else if(err.code == 'ENOENT') {
+    fs.writeFile(filePath, '[]', function(err) {
+      if(err) {
+        return console.log(err);
+      }
+      console.log('File created');
     });
+  } else {
+    console.log('Some other error: ', err.code);
+  }
 });
 
-//get comment by id
-router.get('/:id', (req, res) => {
-    Comment.findById(req.params.id, (err, comment) => {
-        if (err) {
-            res.send('Error has occurred');
-        } else {
-            res.send(comment);
-        }
-    });
+//get comments
+app.get('/api/comments', function(req, res) {
+  fs.readFile(filePath, function(err, data) {
+    if(err) {
+      return console.log(err);
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  });
 });
 
-//get all comments by post id
-router.get('/post/:id', (req, res) => {
-    Comment.find({postId: req.params.id}, (err, comments) => {
-        if (err) {
-            res.send('Error has occurred');
-        } else {
-            res.send(comments);
-        }
+//add a comment
+app.post('/api/comments', function(req, res) {
+  fs.readFile(filePath, function(err, data) {
+    if(err) {
+      return console.log(err);
+    }
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile(filePath, JSON.stringify(comments), function(err) {
+      if(err) {
+        return console.log(err);
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(comments));
     });
+  });
 });
 
-//get all comments by user id
-router.get('/user/:id', (req, res) => {
-    Comment.find({userId: req.params.id}, (err, comments) => {
-        if (err) {
-            res.send('Error has occurred');
-        } else {
-            res.send(comments);
-        }
+//delete a comment
+app.delete('/api/comments/:id', function(req, res) {
+  fs.readFile(filePath, function(err, data) {
+    if(err) {
+      return console.log(err);
+    }
+    var comments = JSON.parse(data);
+    comments.splice(req.params.id, 1);
+    fs.writeFile(filePath, JSON.stringify(comments), function(err) {
+      if(err) {
+        return console.log(err);
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(comments));
     });
+  });
 });
 
-//add new comment
-router.post('/', (req, res) => {
-    var comment = new Comment(req.body);
-    comment.save((err) => {
-        if (err) {
-            res.send('Error has occurred');
-        } else {
-            res.send('Comment added successfully');
-        }
-    });
-});
-
-//update comment by id
-router.put('/:id', (req, res) => {
-    Comment.findByIdAndUpdate(req.params.id, req.body, (err) => {
-        if (err) {
-            res.send('Error has occurred');
-        } else {
-            res.send('Comment updated successfully');
-        }
-    });
-});
-
-//delete comment by id
-router.delete('/:id', (req, res) => {
-    Comment.findByIdAndDelete(req.params.id, (err) => {
-        if (err) {
-            res.send('Error has occurred');
-        } else {
-            res.send('Comment deleted successfully');
-        }
-    });
-});
-
-module.exports = router;
+//update a comment
+app.put('/api/comments/:id', function(req, res) {
+  fs.readFile(filePath, function(err, data) {
+    if(err) {
+      return console.log(err);
+    }
+    var comments = JSON.parse(data);
+    comments[req
